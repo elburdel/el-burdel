178f46f162dd203eb13603309ae489b018eb96f9
elburdel <elburdel390@gmail.com>
2026-05-08 21:32:18 -0300

    Update impostor.js

--- games/impostor.js
+++ games/impostor.js
@@ -14,32 +14,86 @@ export const WORD_BANK = {
   "Lugares": [
     "Playa", "Montaña", "Aeropuerto", "Hospital", "Supermercado",
     "Zoológico", "Estadio", "Biblioteca", "Casino", "Restaurante",
-    "Escuela", "Cementerio", "Parque", "Discoteca", "Museo"
+    "Escuela", "Cementerio", "Parque", "Discoteca", "Museo",
+    "Peluquería", "Ferretería", "Farmacia", "Comisaría", "Iglesia",
+    "Tren", "Puerto", "Cuartel", "Embajada", "Circo"
   ],
   "Objetos": [
     "Heladera", "Televisor", "Paraguas", "Linterna", "Termómetro",
     "Brújula", "Escalera", "Espejo", "Maletín", "Telescopio",
-    "Microscopio", "Calculadora", "Mochila", "Candado", "Sacacorchos"
+    "Microscopio", "Calculadora", "Mochila", "Candado", "Sacacorchos",
+    "Ancla", "Boomerang", "Palanca", "Sextante", "Metrónomo",
+    "Ventosa", "Tridente", "Catafalco", "Bisturí", "Desfibrilador"
   ],
   "Comidas": [
     "Milanesa", "Empanada", "Asado", "Sushi", "Pizza",
     "Helado", "Medialunas", "Locro", "Humita", "Alfajor",
-    "Torta", "Flan", "Churrasco", "Ravioles", "Choripán"
+    "Torta", "Flan", "Churrasco", "Ravioles", "Choripán",
+    "Mondongo", "Matambre", "Provoleta", "Facturas", "Chimichurri",
+    "Carbonada", "Puchero", "Revuelto Gramajo", "Dulce de Leche", "Mazamorra"
   ],
   "Animales": [
     "Pingüino", "Cocodrilo", "Flamenco", "Murciélago", "Camaleón",
     "Pulpo", "Nutria", "Cóndor", "Jaguar", "Delfín",
-    "Avestruz", "Tortuga", "Loro", "Castor", "Mantarraya"
+    "Avestruz", "Tortuga", "Loro", "Castor", "Mantarraya",
+    "Ornitorrinco", "Axolote", "Tarántula", "Anaconda", "Capibara",
+    "Glotón", "Narval", "Komodo", "Pangolín", "Tapir"
   ],
   "Profesiones": [
     "Astronauta", "Sommelier", "Taxidermista", "Carnicero", "Árbitro",
-    "Domador", "Buzos", "Locutor", "Marionetista", "Geólogo",
-    "Bombero", "Piloto", "Cirujano", "Detective", "Mago"
+    "Domador", "Buzo", "Locutor", "Marionetista", "Geólogo",
+    "Bombero", "Piloto", "Cirujano", "Detective", "Mago",
+    "Trapecista", "Tasador", "Embalsamador", "Crupier", "Pirotécnico",
+    "Cartógrafo", "Herrero", "Ventrílocuo", "Espía", "Verdugo"
   ],
   "Películas / Series": [
     "Titanic", "Matrix", "El Padrino", "Pulp Fiction", "Breaking Bad",
     "Game of Thrones", "La Casa de Papel", "Avatar", "Interstellar", "Joker",
-    "El Rey León", "Friends", "Squid Game", "Inception", "Gladiador"
+    "El Rey León", "Friends", "Squid Game", "Inception", "Gladiador",
+    "Chernobyl", "Oppenheimer", "Succession", "True Detective", "Narcos",
+    "El Silencio de los Inocentes", "Mad Max", "The Wire", "Severance", "Euphoria"
+  ],
+  "Deportes": [
+    "Polo", "Rugby", "Waterpolo", "Esgrima", "Curling",
+    "Sumo", "Kendo", "Canotaje", "Biatlón", "Pelota Vasca",
+    "Críquet", "Lacrosse", "Squash", "Pentatlón", "Bobsled",
+    "Escalada", "Skate", "Natación Sincronizada", "Tiro con Arco", "Lucha Grecorromana"
+  ],
+  "Personajes Históricos": [
+    "Napoleón", "Cleopatra", "Einstein", "Darwin", "Freud",
+    "Maradona", "Evita", "San Martín", "Borges", "Gardel",
+    "Gandhi", "Julio César", "Marco Polo", "Copérnico", "Espartaco",
+    "Vlad el Empalador", "Rasputín", "Atila", "Juana de Arco", "Gengis Kan"
+  ],
+  "Cosas que se hacen con las manos": [
+    "Tejer", "Amasar", "Aplaudir", "Escribir a máquina", "Tocar el piano",
+    "Hacer origami", "Soldar", "Esculpir", "Malabarismo", "Boxear",
+    "Hacer señas", "Tatuar", "Afeitar", "Manicuría", "Cirugía",
+    "Tocar la guitarra", "Hacer point", "Manejar", "Hacer punto", "Abrir una caja fuerte"
+  ],
+  "Eventos / Situaciones": [
+    "Casamiento", "Velorio", "Asado", "Cumpleaños", "Juicio",
+    "Examen", "Entrevista de trabajo", "Bautismo", "Divorcio", "Graduación",
+    "Primer día de trabajo", "Cita a ciegas", "Secuestro", "Motín", "Operativo policial",
+    "Maratón", "Consejo de guerra", "Subasta", "Desfile militar", "Sepelio"
+  ],
+  "Cosas que se encuentran en...": [
+    "Una heladería", "Un barco pirata", "Una sala de cirugía", "Un casino",
+    "Una iglesia", "Un zoológico", "Una cárcel", "Un circo",
+    "Una cueva", "Un submarino", "Una farmacia", "Un cuartel",
+    "Un velorio", "Una cancha de fútbol", "Una estación espacial",
+    "Una peluquería", "Un laboratorio secreto", "Un búnker", "Una morgue", "Un palacio"
+  ],
+  "Música": [
+    "Cumbia", "Tango", "Reggaetón", "Ópera", "Heavy Metal",
+    "Bossa Nova", "Flamenco", "Jazz", "Trap", "Folklore",
+    "Vals", "Salsa", "Murga", "Cuarteto", "Candombe",
+    "Funk", "Gospel", "Techno", "Bolero", "Chamamé"
+  ],
+  "Cosas que dan miedo": [
+    "Payaso", "Muñeco de ventrílocuo", "Sótano oscuro", "Araña gigante", "Palmada en el hombro a las 3am",
+    "Dentista", "Examen de conducir", "Suegra", "Deuda en dólares", "Silencio en el grupo familiar",
+    "Nota del colegio", "Cita a ciegas", "Resultado del médico", "El jefe que 'quiere charlar'", "Mensaje de voz de 8 minutos"
   ]
 };
 
